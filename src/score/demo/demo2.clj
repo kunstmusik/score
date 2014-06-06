d8bf2b1d6b9593b0dcfb4ccbbc3b9efa81f09cac
Steven Yi <stevenyi@gmail.com>
2014-06-06 14:24:51 -0400

    implemented CMask oscillator generators

---
+++ src/score/demo/demo2.clj
@@ -0,0 +1,30 @@
+(ns score.demo.demo2
+  (:require 
+    [score.core :refer :all]
+    [score.oscillators :refer :all]
+    [score.util :refer [debug-print]]
+    ))
+
+
+;; Test Code
+
+(defn gen-test-score 
+  [instr fn1 fn2]
+  (format-sco 
+    (gen-score2 0.0 2.0 
+                (const instr) (const 0.1) 
+                fn1 fn2)))
+
+(println "[OSCILLATOR TEST]\n")
+(println "\n[TEST SIN/COS]")
+(println (gen-test-score 1 (sin 0.5 0.5) (cos 2.0 0.5)))
+
+(println "\n[TEST SAW UP/DOWN]")
+(println (gen-test-score 1 (saw-up 2.0 0.5) (saw-down 2.0 0.5)))
+
+(println "\n[TEST SQUARE/TRIANGLE]")
+(println (gen-test-score 1 (square 3.0) (triangle 3.0 0.5)))
+
+(println "\n[TEST POWER UP/DOWN]")
+(println (gen-test-score 1 (power-up 1.5 0.5) 
+                         (power-down 1.5 0.5)))
---
+++ src/score/oscillators.clj
@@ -0,0 +1,106 @@
+(ns ^{:author "Steven Yi"
+      :doc "Oscillator Generators, based on Andre Bartetzki's CMask" } 
+  score.oscillators)
+
+(def PI2 (* Math/PI 2.0))
+
+(defn- get-phase 
+  [phaseinit t freq]
+  (+ phaseinit (* t freq)))
+
+(defn sin 
+  "Sine-wave generator"
+  ([]
+   (sin 1.0 0.0))
+  ([freq]
+   (sin freq 0.0))
+  ([freq phase]
+   (fn [t]
+    (+ 0.5 
+       (* 0.5 
+          (Math/sin (* PI2 (get-phase phase t freq))))))))
+
+
+(defn cos 
+  "Cosine-wave generator"
+  ([]
+   (sin 1.0 0.0))
+  ([freq]
+   (sin freq 0.0))
+  ([freq phase]
+   (fn [t]
+    (+ 0.5 
+       (* 0.5 
+          (Math/cos (* PI2 (get-phase phase t freq))))))))
+
+
+(defn saw-up 
+  "Sawtooth (upwards) generator"
+  ([]
+   (saw-up 1.0 0.0))
+  ([freq]
+   (saw-up freq 0.0))
+  ([freq phase]
+  (fn [t]
+    (Math/abs (rem (get-phase phase t freq) 1.0))
+    )))
+
+(defn saw-down 
+  "Sawtooth (downwards) generator"
+  ([]
+   (saw-up 1.0 0.0))
+  ([freq]
+   (saw-up freq 0.0))
+  ([freq phase]
+   (fn [t]
+     (- 1.0 (Math/abs (rem (get-phase phase t freq) 1.0))))))
+
+(defn power-up
+  "Power up generator"
+  ([]
+   (power-up 1.0 0.0 1.0))
+  ([freq]
+   (power-up freq 0.0 1.0))
+  ([freq phase]
+   (power-up freq phase 1.0))
+  ([freq phase exponent]
+   (fn [t]
+     (Math/pow (Math/abs (rem (get-phase phase t freq) 1.0))
+               (Math/pow 2.0 exponent)))))
+
+(defn power-down 
+  "Power down generator"
+  ([]
+   (power-down 1.0 0.0 1.0))
+  ([freq]
+   (power-down freq 0.0 1.0))
+  ([freq phase]
+   (power-down freq phase 1.0))
+  ([freq phase exponent]
+   (fn [t]
+     (Math/pow (- 1.0 (Math/abs (rem (get-phase phase t freq) 1.0)))
+               (Math/pow 2.0 exponent)))))
+
+(defn square 
+  "Square-wave generator"
+  ([]
+   (square 1.0 0.0))
+  ([freq]
+   (square freq 0.0))
+  ([freq phase]
+   (fn [t]
+     (let [x (Math/abs (rem (get-phase phase t freq) 1.0))]  
+       (if (< x 0.5) 1.0 0.0)))))
+
+(defn triangle 
+  "Triangle-wave generator"
+  ([]
+   (triangle 1.0 0.0))
+  ([freq]
+   (triangle freq 0.0))
+  ([freq phase]
+   (fn [t]
+     (let [x (Math/abs (rem (get-phase phase t freq) 1.0))]  
+       (if (< x 0.5)
+         (* 2 x)
+         (* 2 (- 1.0 x)))))))
