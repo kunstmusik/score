df90eef266d0c89881e904a711ab1f1aa4af6e70
Steven Yi <stevenyi@gmail.com>
2014-06-06 14:24:37 -0400

    added debug-print generator wrapper

--- src/score/util.clj
+++ src/score/util.clj
@@ -9,3 +9,12 @@
         (swap! curval rest) 
         a
         ))))
+
+(defn debug-print
+  "Decorates a generator function. Prints the generated value and passes through the value."
+  [f]
+  (fn [t]
+    (let [v (f t)]
+      (println ">>> " v) 
+      v
+      )))
