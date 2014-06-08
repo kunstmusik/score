4585096e0c6f132371bfd62f7cd2b80a40bcd618
Steven Yi <stevenyi@gmail.com>
2014-06-07 23:25:41 -0400

    wrapped args with wrap-generator

--- src/score/probability.clj
+++ src/score/probability.clj
@@ -1,4 +1,9 @@
-(ns score.probability)
+(ns ^{
+      :author "Steven Yi"
+      :doc "Probability functions. Based on Andre Bartetzki's CMask." } 
+  score.probability
+  (:require [score.core :refer [wrap-generator]])
+  )
 
 (defn uniform
   "uniform random generator with range 0.0 to 1.0"
@@ -6,7 +11,8 @@
   (fn [t] (rand)))
 
 (defn linear
-  "linear distribution from 0.0 to 1.0 (if direction 1), or 1.0 to 0.0 (if direction -1)" 
+  "linear distribution from 0.0 to 1.0 (if direction 1), or 1.0 to 0.0 (if 
+  direction -1)" 
   [direction]
   (fn [t]
     (let [x1 (rand) 
@@ -20,8 +26,8 @@
   []
   (fn [t] 
     (let [x1 (rand) 
-        x2 (rand)]
-    (* 0.5 (+ x1 x2)))))
+          x2 (rand)]
+      (* 0.5 (+ x1 x2)))))
 
 (defn- rand-not [not-arg]
   (let [val (rand)]
@@ -32,87 +38,96 @@
 
 (defn exponential
   "exponential distribution
-   direction - can be :decreasing, :increasing, or :bilateral
-   lambda - affects exponential curvature"
+  direction - can be :decreasing, :increasing, or :bilateral
+  lambda - affects exponential curvature (can be bpf, must be > 0.0)"
   [direction lambda]
-  (if (= direction :bilateral)
-    (fn [t]
-      (let [x (* 2.0 (rand))]
-        (if (> x 1.0)
-          (loop [] 
-            (let [a (- 2.0 x)
-                e (- (Math/log a))
-                ret (+ 0.5 (/ e 14.0 lambda))]
-              (if (or (> ret 1.0) (< ret 0))
-                (recur)
-                ret 
-                )))
-          (loop [] 
-            (let [e (Math/log x) 
-                ret (+ 0.5 (/ e 14.0 lambda))]
-              (if (or (> ret 1.0) (< ret 0))
-                (recur)
-                ret 
-                )))))) 
-    (fn [t]
-      (let [x (/ (- (Math/log (rand-not 0.0))) 7.0 lambda)] 
-        (if (> x 1.0)
-          (recur t)
-          (if (= direction :increasing)
-            (- 1.0 x)
-            x
-            ))))))
+  (let [lambdafn (wrap-generator lambda)]
+    (if (= direction :bilateral)
+      (fn [t]
+        (let [x (* 2.0 (rand))]
+          (if (> x 1.0)
+            (loop [] 
+              (let [a (- 2.0 x)
+                    e (- (Math/log a))
+                    ret (+ 0.5 (/ e 14.0 (lambdafn t)))]
+                (if (or (> ret 1.0) (< ret 0))
+                  (recur)
+                  ret 
+                  )))
+            (loop [] 
+              (let [e (Math/log x) 
+                    ret (+ 0.5 (/ e 14.0 (lambdafn t)))]
+                (if (or (> ret 1.0) (< ret 0))
+                  (recur)
+                  ret 
+                  )))))) 
+      (fn [t]
+        (let [x (/ (- (Math/log (rand-not 0.0))) 7.0 (lambdafn t))] 
+          (if (> x 1.0)
+            (recur t)
+            (if (= direction :increasing)
+              (- 1.0 x)
+              x
+              )))))))
 
 
 (defn gauss 
   "gaussian distribution, takes sigma and mu"
   [sigma mu]
-  (fn [t] 
-    (let [sum (reduce +  (take 12  (repeatedly rand))) 
-        e (+ mu (* sigma (- sum 6.0)))]
-      (if (or (> e 1.0) (< e 0.0))
-        (recur t)
-        e
-        ))))
+  (let [sigmafn (wrap-generator sigma)
+        mufn (wrap-generator mu)]
+    (fn [t] 
+      (let [sum (reduce +  (take 12  (repeatedly rand))) 
+            e (+ (mufn t) (* (sigmafn t) (- sum 6.0)))]
+        (if (or (> e 1.0) (< e 0.0))
+          (recur t)
+          e
+          )))))
 
 
 (defn cauchy 
   "cauchy distribution, takes sigma and mu"
   [alpha mu]
-  (fn [t] 
-    (loop [] 
-      (let [x (rand-not 0.5) 
-          e (+ mu (* alpha (Math/tan (* x Math/PI))))]
-      (if (or (> e 1.0) (< e 0.0))
-        (recur)
-        e
-        )))))
+  (let [alphafn (wrap-generator alpha)
+        mufn (wrap-generator mu)]
+    (fn [t] 
+      (loop [] 
+        (let [x (rand-not 0.5) 
+              e (+ (mufn t) (* (alphafn t) (Math/tan (* x Math/PI))))]
+          (if (or (> e 1.0) (< e 0.0))
+            (recur)
+            e
+            ))))))
 
 
 (defn beta 
-  "beta distribution, takes a and b"
+  "beta distribution, takes a {0..1} and b {0..1}, both can be bpf."
   [a b]
-  (fn [t] 
-    (loop [] 
-      (let [x1 (rand) 
-            x2 (rand)
-            yps1 (Math/pow x1 (/ 1.0 a))
-            yps2 (Math/pow x2 (/ 1.0 b))
-            sum (+ yps1 yps2)]
-        (if (> sum 1.0) 
-          (recur)
-          sum 
-          )))))
+  (let [afn (wrap-generator a)
+        bfn (wrap-generator b)]
+    (fn [t] 
+      (loop [] 
+        (let [x1 (rand) 
+              x2 (rand)
+              yps1 (Math/pow x1 (/ 1.0 (afn t)))
+              yps2 (Math/pow x2 (/ 1.0 (bfn t)))
+              sum (+ yps1 yps2)]
+          (if (> sum 1.0) 
+            (recur)
+            sum 
+            ))))))
 
 (defn weibull
-  "weibull distribution, takes s and t"
+  "weibull distribution, takes s {0..1} and t (>0), both can be bpf."
   [s t]
-  (fn [t]
-    (loop []
-      (let [x (rand)
-            a (/ 1.0 (- 1.0 x)) 
-            e (* s (Math/pow (Math/log a) (/ 1.0 t)))] 
-        (if (> e 1.0)
-          (recur)
-          e
-          )))))
+  (let [sfn (wrap-generator s)
+        tfn (wrap-generator t)]
+    (fn [t]
+      (loop []
+        (let [x (rand)
+              a (/ 1.0 (- 1.0 x)) 
+              e (* (sfn t) (Math/pow (Math/log a) (/ 1.0 (tfn t))))] 
+          (if (> e 1.0)
+            (recur)
+            e
+            ))))))
