eaa1c99d4574dc50a5b1e5d36a7bea24b4d27297
Steven Yi <stevenyi@gmail.com>
2015-05-07 16:57:51 -0400

    reworked sieves: changed from multimethod to protocol design, using defrecord for Union and Intersections; first working implementation

--- src/score/sieves.clj
+++ src/score/sieves.clj
@@ -1,65 +1,28 @@
 (ns score.sieves
-  "Functions related to Xenakis Sieves"
+  "Sieve functions based on work by Iannis Xenakis. Sieves are represented as
+  a 2-vector of [modulo index].  U and I are used to create Union and
+  Intersection Sieves. Sieves may also be nil, though are factored out when a
+  Sieve is simplified, and generates no sequence when gen-sieve is called.
+  
+ Code Consulted:
+   * C Code from \"Sieves\" article below.
+   * Haskell Music Theory (https://hackage.haskell.org/package/hmt-0.15) 
+   * athenaCL (http://www.flexatone.org/athena.html) 
+ 
+ Literature:
+   * Xenakis and Rahn. \"Sieves\". Perspectives of New Music, Vol. 28, No. 1
+   (Winter, 1990), pp. 58-78.
+  "
   (:require [clojure.java.io :refer :all]
             [clojure.string :refer [trim triml split]]))
 
-;; Sieve is represented as vector of [m i] where m is the modulus and
-;; i is the initial offset
+;; NOTES
+;; 
 
+;; UTILITY FUNCTIONS
 
-(defn U 
-  "Create new sieve that is the union of sieves"
-  [& sieves]
-  {:tag :union
-   :items sieves})
-
-(defn I
-  "Create new sieve that is the intersection of sieves"
-  [& sieves]
-  {:tag :intersection
-   :items sieves})
-
-(defmulti element? :tag)
-
-(defmethod element? :union [s n]
-  (let [sieves (:items s)]
-    (some #(element? % n) sieves)))
-
-(defmethod element? :intersection [s n]
-   (let [sieves (:items s)]
-    (every? #(element? % n) sieves)))
-
-(defmethod element? :default [s n]
-  (if-not (vector? s)
-    (throw (Exception. (str "Invalid sieve: " s))))
-  (let [[m i] s]
-    (= i (mod n m))))
-
-
-(defmulti normalize :tag)
-
-(defmethod normalize :union [s]
-  (apply U (map normalize (:items s))))
-
-(defmethod normalize :intersection [s]
-  (apply I (map normalize (:items s))))
-
-(defmethod normalize :default [s]
-  (let [[m i] s]
-    [m (mod i m)]))
-
-(defn- gcd
-  "Greated Common Divisor"
-  [a b]
-  )
-
-(defn- lcm 
-  "Least multiple"
-  [a b]
-  )
-
-(defn- euclide
-  ;a1 >= a2 > 0
+(defn- euclid 
+  "Euclid's algorithm for computing the greatest common divisor"
   [a1 a2]
   (loop [a1 a1 a2 a2]
     (let [tmp (mod a1 a2)] 
@@ -67,8 +30,12 @@
       a2 
       (recur a2 tmp)))))
 
+;(defn- lcm 
+;  "Least multiple"
+;  [a b]
+;  (/ (* a b) (euclid a b)))
+
 (defn- meziriac 
-  ;c1 >= c2 > 0
   [c1 c2]
   (if (= c2 1)
     1
@@ -77,8 +44,101 @@
         t
         (recur (unchecked-inc t))))))
 
-(defn gen-sieve 
-  [sieve]
-  (filter #(element? sieve %) (range)))
+(defn- reduce-intersection
+  [s1 s2]
+  (let [[m1 i1] s1
+        [m2 i2] s2
+        d (euclid m1 m2)
+        i1' (mod i1 m1)
+        i2' (mod i2 m2)
+        c1 (/ m1 d)
+        c2 (/ m2 d)
+        m3 (* d c1 c2)
+        t (meziriac c1 c2)
+        i3 (mod (+ i1' (* t (- i2' i1') c1)) m3)]
+    (if (and (not= d 1) (not= (mod (- i1' i2') d) 0))
+      nil
+      [m3 i3])))
 
+;; Sieve is represented as vector of [m i] where m is the modulus and
+;; i is the initial offset
+
+(defprotocol Sieve 
+  "Protocol for Sieves"
+  (element? [s n] "Determines if number n is an element of this Sieve")
+  (reduce-sieve [s] "Reduces the Sieve to its simplest form")
+  (normalize [s] "Reduces Sieves in [mod index] form to its normalized form"))
+
+(defn- reduce-aggregate
+  [aggregate-fn l r]
+  (let [l' (reduce-sieve l)
+        r' (reduce-sieve r)
+        s' (aggregate-fn l' r')]
+    (cond
+      (nil? l') r'
+      (nil? r') l'
+      (and (= l l') (= r r')) s'
+      :default (reduce-sieve s'))))
+
+(defrecord Union [l r]
+  Sieve
+  (element? [s n]
+    (or (element? l n) (element? r n)))
+  (reduce-sieve [s]
+    (reduce-aggregate ->Union l r))
+  (normalize [s]
+    (Union. (normalize l) (normalize r))))
+
+(defrecord Intersection [l r]
+  Sieve 
+  (element? [s n]
+    (and (element? l n) (element? r n)))
+  (reduce-sieve [s]
+    (cond
+      (nil? r) l
+      (= l r) l
+      (and (vector? l) (vector? r))
+      (reduce-intersection l r)
+      :default
+      (reduce-aggregate ->Intersection l r)))
+  (normalize [s]
+    (Intersection. (normalize l) (normalize r))))
+
+(extend-type nil
+  Sieve
+  (element? [s n] false)  
+  (reduce-sieve [s] nil)
+  (normalize [s] nil))
+
+(extend-type clojure.lang.PersistentVector 
+  Sieve
+  (element? [s n] 
+    (let [[m i] s] 
+      (= i (mod n m))))  
+  (reduce-sieve [s] s)
+  (normalize [s] 
+    (let [[m i] s] 
+      [m (mod i m)])))
+
+(defn U 
+  "Create new sieve that is the union of sieves"
+  [& sieves]
+  (reduce ->Union sieves))
+
+(defn I
+  "Create new sieve that is the intersection of sieves"
+  [& sieves]
+  (reduce ->Intersection sieves))
+
+(defn simplified 
+  "Returns simplified (normalized and reduce) version of sieve"
+  [sieve]
+  (reduce-sieve (normalize sieve)))
+
+(defn gen-sieve 
+  "Generate infinite sequence using sieve."
+  [sieve]
+  (let [s (simplified sieve)]
+    (when-not (nil? s)
+      (filter #(element? sieve %) (range)))))
 
--- test/score/sieves_test.clj
+++ test/score/sieves_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [score.sieves :refer :all]))
 
+(defmacro with-private-fns  [[ns fns] & tests]
+    "Refers private fns from ns and runs tests in context."
+      `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2))  [] fns)
+              ~@tests))
+
+
 (deftest gen-sieve-test
   (testing "Generation of sequences with sieves"
     (is (= [0 2 4 6] (take 4 (gen-sieve [2 0])))) 
@@ -9,6 +15,12 @@
            (take 10 (gen-sieve (U [2 1] [3 2]))))) 
     (is (= [5 11 17 23 29 35 41 47 53 59] 
            (take 10 (gen-sieve (I [2 1] [3 2]))))) 
+    (is (nil? (gen-sieve (I [2 1] [2 2])))) 
+    (is (= [5 11 17 23 29 35 41 47 53 59] 
+           (take 10 
+                 (gen-sieve (U (I [2 1] [3 2])
+                                  (I [2 1] [2 2])
+                                  )))))
     ))
 
 (deftest normalize-test
@@ -22,3 +34,28 @@
     (is (= (I [4 1] (U [3 0] [4 3])) 
            (normalize (I [4 5] (U [3 9] [4 7]))))) 
     ))
+
+(deftest gcd-test
+  (with-private-fns [score.sieves [gcd]] 
+    (testing "GCD using Euclid's Algorithm"
+    (is (= 4 (gcd 16 4)))
+    (is (= 21 (gcd 252 105)))
+    (is (= 21 (gcd 105 252)))
+    ))
+  )
+
+(deftest reduce-sieve-test
+  (testing "reduce-sieve"
+    (is (= [4 3] (reduce-sieve (I [4 3] nil))))
+    (is (= [4 3] (reduce-sieve (I nil [4 3]))))
+    (is (= [4 3] (reduce-sieve (I [4 3] [4 3]))))
+    (is (= [8 3] (reduce-sieve (I [8 3] [4 3]))))
+    (is (nil? (reduce-sieve (I [8 3] [8 4]))))
+
+    (is (= [4 3] (reduce-sieve (U [4 3] nil))))
+    (is (= [4 3] (reduce-sieve (U nil [4 3]))))
+    (is (nil? (reduce-sieve (U nil (I [8 3] [8 4])))))
+    (is (nil? (reduce-sieve (U (I [8 3] [8 4]) nil))))
+    ))
+
+
