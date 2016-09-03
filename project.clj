ce7afa90d06ae6446610410b2394f7e591ac1bab
Steven Yi <stevenyi@gmail.com>
2016-09-03 12:03:10 -0400

    updated to use lein-codox, restructured source to be more maven-like and use demo folder only with :dev profile

--- project.clj
+++ project.clj
@@ -3,10 +3,14 @@
   :url "http://github.com/kunstmusik/score"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
-  :profiles  { :dev  {
-                      :global-vars  {*warn-on-reflection* true} } 
+  :profiles  { 
+              :dev  {
+                     :global-vars  {*warn-on-reflection* true} 
+                     :plugins [[lein-codox "0.9.6"]] 
+                     :source-paths ["src/demo"]
+                     } 
 
               :profiling  {
                            :plugins  [[lein-nodisassemble "0.1.3"]] 
@@ -14,5 +18,11 @@
                            :global-vars  {*warn-on-reflection* true
                                           *unchecked-math* :warn-on-boxed }} 
               }
+
+  :source-paths  ["src/main"] 
+  :test-paths  ["src/test"]
+  :javac-options     ["-target" "1.7" "-source" "1.7"]
   :scm {:name "git"
-        :url "https://github.com/kunstmusik/score.git" })
+        :url "https://github.com/kunstmusik/score.git" }
+  :codox {:source-paths ["src/main"] } 
+  )
